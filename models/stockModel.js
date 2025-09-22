ec9090026843d1db7ba2d92bdca5e7a5fb7f6b32
keilar kirabira <kirabirakeilarlisa@gmail.com>
2025-09-21 23:08:41 -0700

    Installed passport-local-mongoose,connect-mongo.

---
+++ models/stockModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const stockSchema = new mongoose.Schema({
+    productName:{
+        type:String,
+        required:true 
+    },
+    productType:{
+       type:String,
+       required:true,
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    costPrice:{
+        type:Number,
+        required:true
+    },
+     productPrice:{
+        type:Number,
+        required:true
+    },
+    supplierName:{
+        type:String,
+        required:true
+    },
+    dateBought:{
+        type:Date,
+        required:true
+    },
+    quality:{
+        type:String,
+        required:true
+    },
+    color:{
+        type:String,
+    },
+    measurements:{
+        type:String,
+    }
+});
+
+module.exports = mongoose.model("StockModel",stockSchema)
--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const StockModel = require("../models/stockModel")
 router.get("/stock", (req, res)=>{
     res.render("stock");
 });
 
-router.post("/stock", (req, res)=>{
-   console.log(req.body);
+router.post("/stock", async (req, res)=>{
+    try {
+        const stock = new StockModel(req.body)
+        console.log(req.body);
+        await stock.save();
+        res.redirect("/dashboard");
+    } catch (error) {
+        console.error(error);
+        res.redirect("/stock");
+    }
 });
 
 
