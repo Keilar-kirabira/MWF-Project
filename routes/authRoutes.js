8284a322e90a5cb2f40d1f896edb72282729b4d3
keilar kirabira <kirabirakeilarlisa@gmail.com>
2025-09-21 23:07:23 -0700

    added authentication using register method.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,39 +1,39 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
 
-const UserModel = require("../models/userModel")
+const UserModel = require("../models/userModel");
 //getting a signup form
-router.get("/signup",(req,res) => {
-    res.render("signup");
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-router.post("/signup",(req,res) => {
+router.post("/signup", async (req, res) => {
+  try {
     const user = new UserModel(req.body);
     console.log(req.body);
-    user.save()
-    res.redirect("/login");
+    let existingUser = await UserModel.findOne({email:req.body.email});
+    if(existingUser){
+      return res.status(400).send("Already registered email.")
+    }else{
+       await UserModel.register(user, req.body.password,(error)=>{
+        if(error){
+           throw error; 
+        }
+        res.redirect("/login");
+       })  
+    }
+  } catch (error) {
+    res.status(400).send("Try again.")
+  }
 });
 
-
-
-
-router.get("/login",(req,res) => {
-    res.render("login")
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.post("/login",(req,res) => {
-    console.log(req.body);
-    res.redirect("/stock");
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/stock");
 });
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
