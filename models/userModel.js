5a03df2e5ef3b81972f961284184298802840275
keilar kirabira <kirabirakeilarlisa@gmail.com>
2025-09-21 23:06:33 -0700

    removed password feild from the schema and named the database collection userModel.

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const passportLocalMongoose = require('passport-local-mongoose');
 const signupSchema = new mongoose.Schema({
     userName:{
         type:String,
@@ -11,14 +11,15 @@ const signupSchema = new mongoose.Schema({
        unique:true,
        trim:true
     },
-    password:{
-        type:String,
-        required:true
-    },
     role:{
         type:String,
         required:true
     }
 });
-
+signupSchema.plugin(passportLocalMongoose,{
+    usernameField:"email"
+});                                                  
 module.exports = mongoose.model("UserModel",signupSchema) 
+
+// am using passport local, for local username and password authentication. but am going against the default to use email instead of username.
+// password is by default so we donot sepecify it here.
